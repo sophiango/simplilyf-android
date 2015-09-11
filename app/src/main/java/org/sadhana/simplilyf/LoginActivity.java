1b3f87045715759dfc248c6dcd17143d24e5f431
Sadhana Pothuganti <Sadhana.106@gmail.com>
2015-09-11 12:12:40 -0700

    with login

---
+++ app/src/main/java/org/sadhana/simplilyf/LoginActivity.java
@@ -0,0 +1,58 @@
+package org.sadhana.simplilyf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private Button mLoginBtn;
+    private EditText mUserName;
+    private EditText mPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mLoginBtn =(Button)findViewById(R.id.btn_login);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this,ShowdevicesActivity.class);
+                startActivity(i);
+            }
+        });
+        mUserName=(EditText)findViewById(R.id.input_username);
+        mPassword=(EditText)findViewById(R.id.input_password);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
