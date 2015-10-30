b5a44b8b7a98be52772c9a009a10c30d129d6b57
Sadhana Pothuganti <Sadhana.106@gmail.com>
2015-10-30 14:29:39 -0700

    added philips

--- app/src/main/java/org/sadhana/simplilyf/PhilipsdevicesActivity.java
+++ app/src/main/java/org/sadhana/simplilyf/PhilipsdevicesActivity.java
@@ -7,16 +7,37 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class PhilipsdevicesActivity extends ActionBarActivity {
+
+    private ListView myList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_philipsdevices);
 
+        myList = (ListView) findViewById(R.id.list);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(NestdevicesActivity.this, "Row " + position + " clicked", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(PhilipsdevicesActivity.this, PhilipsDetailsActivity.class);
+                System.out.print("Starting Intent");
+                startActivity(i);
+            }
+        });
         FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        final ArrayList list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("Item " + i);
+        }
+        final MyCustomAdapter adapter = new MyCustomAdapter(PhilipsdevicesActivity.this, list);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +48,11 @@ public class PhilipsdevicesActivity extends ActionBarActivity {
                 startActivity(i);
             }
         });
+
+        //show the ListView on the screen
+        // The adapter MyCustomAdapter is responsible for maintaining the data backing this list and for producing
+        // a view to represent an item in that data set.
+        myList.setAdapter(adapter);
 
     }
 
