6e55c73486da5b64044bc7accd7a3680c9a2abc2
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-12-03 23:42:43 -0800

    Hosted node server on ec2 so updated elastic IP

--- app/src/main/java/org/sadhana/simplilyf/Config.java
+++ app/src/main/java/org/sadhana/simplilyf/Config.java
@@ -13,9 +13,9 @@ package org.sadhana.simplilyf;
     }
 
     /* Comment out the one you are not using */
-    final String PHILIPS_IP_ADDRESS = "http://10.189.18.218:3000";
-//    final String IP_ADDRESS = "http://52.8.12.57:3000"; // Cloud server
-    final String IP_ADDRESS = "http://10.189.18.218:3000";
+    final String PHILIPS_IP_ADDRESS = "http://10.189.18.218:3000"; // change to IP address of the machine running Philips Hue Emulator
+    final String IP_ADDRESS = "http://52.8.12.57:3000"; // Cloud server
+//    final String IP_ADDRESS = "http://10.189.18.218:3000";
 //    final String IP_ADDRESS = "http://172.16.1.9:3000";
 //    final String IP_ADDRESS = "http://192.168.1.9:3000";
 
